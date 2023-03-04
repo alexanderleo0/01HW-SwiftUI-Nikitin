102cf86c3068f7e951df3d321c669b6883757358
alexanderleo0 <alnikitin3@gmail.com>
2023-03-04 08:36:26 +0300

    add UIViewRepresentable

--- 01HW-SwiftUI-Nikitin/Third/ThirdTabScreen.swift
+++ 01HW-SwiftUI-Nikitin/Third/ThirdTabScreen.swift
@@ -1,34 +1,47 @@
-//
-//  ThirdTabScreen.swift
-//  01HW-SwiftUI-Nikitin
-//
-//  Created by Александр Никитин on 03.03.2023.
-//
+    //
+    //  ThirdTabScreen.swift
+    //  01HW-SwiftUI-Nikitin
+    //
+    //  Created by Александр Никитин on 03.03.2023.
+    //
 
 import SwiftUI
 
 struct ThirdTabScreen: View {
     @State var modalViewIsShow : Bool = false
+    @State var someText: String? = nil
     var body: some View {
         
-        Button {
-            // Show modal view
-            modalViewIsShow.toggle()
-        } label: {
-            Text("Select Random Book")
-                .foregroundColor(.white)
+        VStack {
+            Spacer()
+            TextField(someText: $someText)
+                
+                .frame(maxWidth: 300, maxHeight: 40)
+                .padding()
+                .border(.orange)
+                .clipped()
+            
+            
+            Spacer()
+            Button {
+                    // Show modal view
+                modalViewIsShow.toggle()
+            } label: {
+                Text("Show Random Book \n In Modal View")
+                    .foregroundColor(.white)
+            }
+            .padding()
+            .background {
+                Color.orange
+            }
+            .cornerRadius(15)
+            
+            .sheet(isPresented: $modalViewIsShow, content: {
+                ModalView()
+            })
+            
         }
-        .padding()
-        .background {
-            Color.orange
-        }
-        .cornerRadius(15)
-        
-        .sheet(isPresented: $modalViewIsShow, content: {
-            ModalView()
-        })
     }
-        
 }
 
 
@@ -38,4 +51,23 @@ struct ThirdTabScreen_Previews: PreviewProvider {
     static var previews: some View {
         ThirdTabScreen()
     }
+}
+
+    //MARK: - Обертка для UIKit
+struct TextField: UIViewRepresentable {
+    
+    @Binding var someText: String?
+    
+    func makeUIView(context: Context) -> UITextField {
+        
+        let textField = UITextField()
+        textField.placeholder = "UIKit TextView"
+        return textField
+        
+    }
+    
+    func updateUIView(_ uiView: UITextField, context: Context) {
+        uiView.text = someText
+    }
+
 }
